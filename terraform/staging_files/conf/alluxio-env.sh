b754209f389c468f79f8abf3a1bf9e69dd2becfd
Greg Palmer <greg@Gregs-MacBook-Pro.local>
2022-01-22 13:37:19 -0500

    Changed -XX:InitialHeapSize to -Xms

--- terraform/staging_files/conf/alluxio-env.sh
+++ terraform/staging_files/conf/alluxio-env.sh
@@ -1,9 +1,11 @@
 # File: alluxio-env.sh
 #
 
-export ALLUXIO_MASTER_JAVA_OPTS+=" -XX:InitialHeapSize=64g -XX:MaxHeapSize=64g -XX:+UseConcMarkSweepGC -XX:+PrintGC -XX:+PrintGCDateStamps -XX:+PrintGCDetails -XX:+PrintGCTimeStamps -XX:+UseParNewGC -Xloggc:/opt/alluxio/logs/jvm_gc_master.log"
+# Alluxio Master Nodes:
+export ALLUXIO_MASTER_JAVA_OPTS+=" -Xms64g -Xmx64g -XX:+UseConcMarkSweepGC -XX:+PrintGC -XX:+PrintGCDateStamps -XX:+PrintGCDetails -XX:+PrintGCTimeStamps -XX:+UseParNewGC -Xloggc:/opt/alluxio/logs/jvm_gc_master.log"
 
-export ALLUXIO_WORKER_JAVA_OPTS+=" -XX:InitialHeapSize=24g -XX:MaxHeapSize=24g -XX:MaxDirectMemorySize=12g -XX:+UseConcMarkSweepGC -XX:+PrintGC -XX:+PrintGCDateStamps -XX:+PrintGCDetails -XX:+PrintGCTimeStamps -XX:+UseParNewGC -Xloggc:/opt/alluxio/logs/jvm_gc_worker.log"
+# Alluxio Worker Nodes:
+export ALLUXIO_WORKER_JAVA_OPTS+=" -Xms24g -Xmx24g -XX:MaxDirectMemorySize=12g -XX:+UseConcMarkSweepGC -XX:+PrintGC -XX:+PrintGCDateStamps -XX:+PrintGCDetails -XX:+PrintGCTimeStamps -XX:+UseParNewGC -Xloggc:/opt/alluxio/logs/jvm_gc_worker.log"
 
 # end of file
 
